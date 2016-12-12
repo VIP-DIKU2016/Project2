2290127583b8f51cbeba7d9c1f497f2037693aaf
Eduards Klavins <klavins.eduards@gmail.com>
2016-12-12 10:35:55 +0100

    Improved usability

--- code.py
+++ code.py
@@ -11,77 +11,92 @@ import matplotlib.pyplot as plt
 def main(argv):
     '''Run the tests'''
     usage = 'Correct usage: \n'
-    usage += '    python code.py <input-image> <method>\n';
+    usage += '    python code.py <method>\n';
     usage += 'Methods available: \n'
     usage += '    1 - Blob detection \n'
     usage += '    2 - Harris corner detection \n'
 
 
     # Check arguments (first argument is invoked name of program)
-    if len(argv) != 3:
+    if len(argv) != 2:
         print usage;
         return 1;
 
-    imagepath = argv[1];
-    methodno = argv[2];
+    imagepaths = ["./Img001_diffuse_smallgray.png","./Img002_diffuse_smallgray.png","./Img009_diffuse_smallgray.png"];
+    methodno = argv[1];
     methodno = int(methodno);
 
-    # Run the programes with sigmas
+    # Run the programes
     if(methodno == 1):
-        blobDetection(imagepath);
+        blobDetection(imagepaths);
     elif(methodno == 2):
-        harrisCorners(imagepath);
+        harrisCorners(imagepaths);
     else:
         print 'Incorrect method number called';
-
-    # show plot
-    plt.show();
   
     # Return 0 to indicate normal termination
     return 0;
 
-def blobDetection(imagepath):
+def blobDetection(imagepaths):
 
     print('Blob detection called');
-    # Read the image from file
-    im = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE);
+    i=1;
+    for imagepath in imagepaths:
+        # Read the image from file
+        im = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE);
 
-    # Set up the detector with default parameters.
-    detector = cv2.SimpleBlobDetector_create();
-     
-    # Detect blobs.
-    keypoints = detector.detect(im);
-     
-    # Draw detected blobs as red circles.
-    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-    im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS);
-     
-    # Show keypoints
-    cv2.imshow("Keypoints", im_with_keypoints)
-    cv2.waitKey(0)
+        # Set up the detector with default parameters.
+        detector = cv2.SimpleBlobDetector_create();
+         
+        # Detect blobs.
+        keypoints = detector.detect(im);
+         
+        # Draw detected blobs as red circles.
+        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+        im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS);
+         
+        # Show keypoints side by side (uncomment 2 lines and the last plt.show after for loop)
+        # plt.subplot(1,3,i),plt.imshow(im_with_keypoints);
+        # plt.title(imagepath), plt.xticks([]), plt.yticks([]);
+        i+=1;
+
+        # Show only one image at a time:
+        plt.imshow(im_with_keypoints);
+        plt.title('Blob detection - '+ imagepath), plt.xticks([]), plt.yticks([]);
+        plt.show();
+    # plt.show();
 
     return 0;
 
-def harrisCorners(imagepath):
+def harrisCorners(imagepaths):
 
     print('Harris corner detection called');
-    # Read the image from file
-    img = cv2.imread(imagepath);
+    i=1;
+    for imagepath in imagepaths:
+        # Read the image from file
+        img = cv2.imread(imagepath);
 
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY);
 
-    gray = np.float32(gray)
-    dst = cv2.cornerHarris(gray,2,3,0.04)
+        gray = np.float32(gray);
+        dst = cv2.cornerHarris(gray,2,3,0.04);
 
-    #result is dilated for marking the corners, not important
-    dst = cv2.dilate(dst,None)
+        #result is dilated for marking the corners, not important
+        dst = cv2.dilate(dst,None);
 
-    # Threshold for an optimal value, it may vary depending on the image.
-    img[dst>0.01*dst.max()]=[0,0,255]
+        # Threshold for an optimal value, it may vary depending on the image.
+        img[dst>0.01*dst.max()]=[255,0,0]
 
-    cv2.imshow('dst',img)
-    if cv2.waitKey(0) & 0xff == 27:
-        cv2.destroyAllWindows()
+        # Show keypoints side by side (uncomment 2 lines and the last plt.show after for loop)
+        # plt.subplot(1,3,i),plt.imshow(img);
+        # plt.title(imagepath), plt.xticks([]), plt.yticks([]);
+        i+=1;
+
+        # Show only one image at a time:
+        plt.imshow(img);
+        plt.title('Corner detection - '+ imagepath), plt.xticks([]), plt.yticks([]);
+        plt.show();
+    # plt.show();
 
     return 0;
 
