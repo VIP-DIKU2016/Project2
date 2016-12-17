3d6c7da54b99320d2d53b1b8fed81b5d9278518f
Sorin Davidoi <sorin.davidoi@gmail.com>
2016-12-17 17:29:10 +0100

    fix(matching): Use square patch instead of SIFT

--- README.txt
+++ README.txt
@@ -1,17 +1,12 @@
 Software needed:
     python 2.7, openCV, scipy, numpy
 
-    We need a build of openCV that supports SIFT (although most likely this is not the right descriptor algorithms to use, see code comments).
-    Did not manage to install it using pip, only with brew:
-    `$ brew install opencv3 --force --with-contrib --HEAD`
-    `$ echo /usr/local/opt/opencv3/lib/python2.7/site-packages >> /usr/local/lib/python2.7/site-packages/opencv3.pth # will be displayed by brew after install`
-
 Correct usage: 
     python code.py <method>
 Methods available: 
     1 - Blob detection  
     2 - Harris corner detection
-    3 - SIFT feature matching
+    3 - Matching
 
 Examples:
     python code.py 1
--- code.py
+++ code.py
@@ -8,6 +8,25 @@ from scipy import ndimage
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def getWindow(width, point, image):
+    '''Returns the patch centers around point'''
+    row, col = point
+
+    row = int(row)
+    col = int(col)
+
+    result = []
+
+    for i in range(width):
+        for j in range(width):
+            x = int(row - width/2 + i)
+            y = int(col - width/2 + j)
+
+            result.append(image[x][y])
+            
+    return result
+
 def main(argv):
     '''Run the tests'''
     usage = 'Correct usage: \n'
@@ -104,23 +123,16 @@ def harrisCorners(imagepaths):
 
     return 0;
 
-def matching(imagepaths):
+def matching(imagepaths, N = 45, k = 2):
 
     print('Matching called')
 
     # Set up the detector with default parameters.
     detector = cv2.SimpleBlobDetector_create()
 
-    # Set up the descriptor
-    # Which one should be use? SIFT has a patch size of 128x128,
-    # and cannot be changed. Which one matches the requirement?
-    # ("For the descriptor you should extract a small square patch, with side length
-    # N, of intensity values centered at the detected point.")
-    descriptor = cv2.xfeatures2d.SIFT_create()
-
     # Set up the matcher
     # Is this the correct distance measurement?
-    matcher = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+    matcher = cv2.BFMatcher(cv2.NORM_L2SQR, crossCheck=False)
 
     def extractDescriptors(imagepath):
         img = cv2.imread(imagepath)
@@ -128,24 +140,52 @@ def matching(imagepaths):
 
         # Detect blobs.
         keypoints = detector.detect(gray)
+        
+        descriptors = []
 
-        # Compute descriptors
-        keypoints, descriptors = descriptor.compute(gray, keypoints)
+        for kp in keypoints:
+            try:
+                # Will throw if the patch extends outside of the image
+                # In this case, discard the descriptors
+                # Should we do something else?
+                w = getWindow(N, kp.pt, gray)
+                descriptors.append(w)
+            except:
+                pass
+
+        descriptors = np.array(descriptors, dtype=np.float32)
 
         return (gray, keypoints, descriptors)
 
+    # The first image is the reference one
     referenceImage, referenceKeypoints, referenceDescriptors = extractDescriptors(imagepaths[0])
 
     for imagepath in imagepaths[1:]:
         image, keypoints, descriptors = extractDescriptors(imagepath)
 
         # Compute the matches between the descriptors
-        matches = matcher.match(descriptors, referenceDescriptors)
+        matches = matcher.knnMatch(descriptors, referenceDescriptors, k)
 
-        # Sort them in the order of their distance.
-        matches = sorted(matches, key = lambda x:x.distance)
+        goodMatches = []
 
-        image = cv2.drawMatches(image, keypoints, referenceImage, referenceKeypoints, matches, np.array([]), flags=2)
+        for descriptorMatches in matches:
+            # Sort them in the order of their distance.
+            sortedDescriptorMatches = sorted(descriptorMatches, key = lambda x:x.distance)
+
+            # TODO
+            # To select a correspondence between a point in image A and points from
+            # image B, you should pick the point pairs with the smallest dissimilarity measure.
+            # However, you may choose not to accept this if:
+            #     The ratio of dissimilarity between the best and the second best match is
+            #     too close to 1 (say above 0.7).
+            #     2. If the best B-image match y to an A-image point x has a best A-image
+            #     match z such that x 6= z (thus left-to-right matching and right-to-left
+            #     matching must agree).
+
+            # Save the best match
+            goodMatches.append(sortedDescriptorMatches[0])
+
+        image = cv2.drawMatches(image, keypoints, referenceImage, referenceKeypoints, goodMatches, np.array([]), flags=2)
 
         plt.title(imagepaths[0] + ' - ' + imagepath),plt.imshow(image),plt.show()
 
